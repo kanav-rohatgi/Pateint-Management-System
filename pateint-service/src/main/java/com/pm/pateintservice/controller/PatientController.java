c5eff0b3507d1bdc288191220ada3dec4b719b07
Kanav Rohatgi <kanavr@gmail.com>
2026-07-11 01:01:02 +0530

    few changes

---
+++ pateint-service/src/main/java/com/pm/pateintservice/controller/PatientController.java
@@ -0,0 +1,26 @@
+package com.pm.pateintservice.controller;
+
+import com.pm.pateintservice.dto.PatientResponseDTO;
+import com.pm.pateintservice.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+
+    @Autowired
+    private PatientService patientService;
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> getPatients() {
+        List<PatientResponseDTO> patients = patientService.getPatients();
+        return ResponseEntity.ok().body(patients);
+    }
+
+}
